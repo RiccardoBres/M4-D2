44aba424d5b35e6bbc89c98915db2818fa2ef963
Riccardo Bresolin <124574800+RiccardoBres@users.noreply.github.com>
2023-05-24 22:51:50 +0100

    completato l'esercizio. Manca l'extra

--- script.js
+++ script.js
@@ -1,44 +1,63 @@
-function search() {
-  //array d'appoggio contenitore dati Json
-  let jsonResponse = [];
-  // variabile contenente il valore del campo Search html
-  let searchButton = document.getElementById("searchField").value.toLocaleLowerCase();
-  // variabile che conterra la sezione degli artisti
-  let artisti = document.getElementsByClassName("artisti");
-  //variabile che permetterà di rimuovere la classe d-none allo scatenarsi dell'evento click, questa varibile contiene qualsiasi valore di searchButton
-  let serchTwo = document.getElementById(searchButton);
-  //la chiamata fetch non va a buon fine.
-  fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem")
-    .then((response) => response.json())
-    .then((data) => {
-      jsonResponse = data;
-      console.log(jsonResponse);
-   //for attraversa ogni elemento con la classe "artisti" e aggiunge la classe "d-none" a ciascuno di essi, nascondendoli nella visualizzazione
-      for (const artists of artisti) {
-        artists.classList.add("d-none");
-      }
-   // rimuovere la classe CSS "d-none" dall'elemento serchTwo quando si verifica l'evento di click su searchButton.
-      // serchTwo.classList.remove("d-none");
+//definiamo una funzione globale che si occuperà di fare la richiesta api. Il parametro sarà la variabile surc
 
-      let serching = document.getElementById(searchButton);
-      for (let i = 0; i < jsonResponse.length; i++) {
-        let card = document.createElement("div");
-        let title = document.createElement("p");
-        let immage = document.createElement("img");
+let data = (searchButton) => {
+  let request = fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=" + searchButton)
+  //promise se risolta entreremo nel then a riga 33 dove la funzione data viene richiamata.Se non viene risolta entriamo nel catch a riga 43
+  let promise = new Promise((resolve, reject) => {
+    resolve(request);
+    reject("errore");
+  })
+  return promise;
+}
+//funzione che permette di creare l'elemento card e lo innietta nel DOM.
+function card(element) {
+  let route = document.getElementById("divDom");
+    let card = document.createElement("div");
+    //aggiunta classe Bootsrap
+    card.classList.add("card");
+    let immage = document.createElement("img");
+    immage.classList.add("img-fluid");
+    //immagine recuperata attraverso l'attributo src iterando iterando la variabile element che contiene il jaison.
+    immage.src = element.album.cover_medium;
+    let title = document.createElement("p");
+    title = element.title;
+    card.append(immage);
+    card.append(title);
+    route.append(card);
 
-        title.innerText = jsonResponse[i].album.title;
-        immage.src = jsonResponse[i].album.cover;
-
-        serching.appendChild(card);
-        card.appendChild(title);
-        card.appendChild(immage);
-      }
-    })
-    .catch((err) => console.log("Error detected: ", err));
+}
+//funzione che si scatena non appena il tasto GO ciene schiacciato. 
+function search () {
+  let div = document.getElementById("divDom");
+  div.innerHTML = "";
+  //variabile surchBotton acquisisce il valore della ricerca nell'input
+  let searchButton = document.getElementById("searchField").value.toLocaleLowerCase()
+  /* Allo scatenarsi della funzione surch, la funzione data(che si occupa di far scatenare la chiamata Fetch, e di conseguenza la promise)
+  viene richiamata e viengono definite le condizioni delle promise */
+  data(searchButton)
+  /*All'interno del primo .then(), 
+  il parametro artisti rappresenta la risposta JSON restituita dalla chiamata API. Viene chiamato .json() 
+  su artisti per convertire la risposta in un oggetto JSON*/ 
+  .then((artisti)=> {
+  artisti = artisti.json();
+  console.log(artisti);
+  return artisti
+})
+/*Viene utilizzato un secondo .then() per gestire la promise restituita dal primo .then(). 
+Il parametro jsonData rappresenta l'oggetto JSON restituito dal primo .then(). Viene eseguito un ciclo .forEach() su jsonData.data, 
+dove jsonData.data è un array contenente gli elementi che si desidera elaborare. 
+All'interno del ciclo, viene eseguita una funzione card() su ciascun elemento per elaborarlo*/  
+.then((jsonData) => jsonData.data.forEach((element) => {
+  console.log(element)
+    card(element)
+}))
+.catch(err => (console.log(err)))
 }
 
 
-search()  
+
+
+
 
 /*function search() {
     let jsonResponse = [];
